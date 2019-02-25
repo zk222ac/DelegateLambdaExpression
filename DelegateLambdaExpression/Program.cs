687da711d694f51f6585609b3bf8855de3afa884
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-25 08:51:30 +0100

    final version

--- DelegateLambdaExpression/Program.cs
+++ DelegateLambdaExpression/Program.cs
@@ -1,21 +1,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DelegateLambdaExpression
 {
+    public delegate int MyDelegateSingleArg(int x);
+    public delegate void MyDelegateMultipleArgs(int x , string y);
+    public delegate bool MyDelegateBool(int x);
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Create the basic delegates that square a number
+            MyDelegateSingleArg foo = (x) => x * x;
+            // what does that (x) means, it takes the input and return square 
+            // (x) -> input 
+            // x * x -> output
+            // => the arrow operator mean such that or goes to 
+            Console.WriteLine($"The result of Foo is : {foo(5)}");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Dynamically change the delegate to something else 
+            foo = (x) => x * 10;
+            Console.WriteLine($"The result of Foo is : {foo(5)}");
+
+            // Create the Delegate that take multiple arguments
+            MyDelegateMultipleArgs bar = (x, y) =>
+            {
+                Console.WriteLine($"Two Lambda Expression -{x * 10} - {y}");
+            };
+            bar(5, "some string data");
+
+            MyDelegateBool boo = (x) => x > 15; 
+
+            Console.ReadKey();
+                
         }
     }
 }
